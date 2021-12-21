e6fb6e29dbb1c9e23699a7568097faf884fb23cd
Kiprey <Kiprey@qq.com>
2021-12-21 08:26:02 +0800

    [fix] fix SSL_test bug

--- SSL_test.cpp
+++ SSL_test.cpp
@@ -21,24 +21,15 @@
     } while(0)
 
 // 默认 CA 证书存放位置
-#define DEFAULT_CA_CER_PATH         "../crt/CA.crt"
+#define DEFAULT_CA_CER_PATH         "../crt/ca.crt"
 // 默认 Server 公钥证书和私钥存放位置
-#define DEFAULT_SERVER_CER_PATH     "../crt/Server.crt"
-#define DEFAULT_SERVER_KEY_PATH     "../crt/Server.key"
+#define DEFAULT_SERVER_CER_PATH     "../crt/server.crt"
+#define DEFAULT_SERVER_KEY_PATH     "../crt/server.key"
 // 默认 Client 公钥证书和私钥存放位置
-#define DEFAULT_CLIENT_CER_PATH     "../crt/Client.crt"
-#define DEFAULT_CLIENT_KEY_PATH     "../crt/Client.key"
+#define DEFAULT_CLIENT_CER_PATH     "../crt/client.crt"
+#define DEFAULT_CLIENT_KEY_PATH     "../crt/client.key"
 
-static SSL_CTX * SSL_init(const string& ca_crt_path, const string& crt_path, const string& key_path) {
-    // 准备好打印调试信息
-    SSL_load_error_strings();
-    ERR_load_crypto_strings();
-    SSL_library_init();
-    // 初始化 SSL
-    OpenSSL_add_ssl_algorithms();
-    // 指定 SSL 协议
-    SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
-
+static void loadcheck_cer(SSL_CTX* ctx, const string& ca_crt_path, const string& crt_path, const string& key_path) {
     // 验证对端的公钥证书
     SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL); 
     // 设置 CA 证书路径
@@ -51,7 +42,6 @@ static SSL_CTX * SSL_init(const string& ca_crt_path, const string& crt_path, con
         printf("Private key does not match the certificate public key\n");
         abort();
     }
-    return ctx;
 }
 
 static void SSL_print_cert(SSL* ssl) {
@@ -86,13 +76,14 @@ static void client_routine(int client_fd, SSL *ssl) {
     SSL_set_fd(ssl, client_fd);
     OPENSSL_ASSERT(SSL_connect(ssl));
     // 输出 SSL 信息
+    cout << "SSL connection using " << SSL_get_cipher(ssl) << endl;
     SSL_print_cert(ssl);
     
     string str;
     while(getline(cin, str)) {
         int wlen = SSL_write(ssl, str.c_str(), str.size());
         int rlen = SSL_read(ssl, buf, sizeof(buf));
-        cout << "[SERVER] " << buf;
+        cout << "[SERVER] " << string(buf, rlen) << endl;
         assert(rlen == wlen);
     }
 	SSL_shutdown(ssl);
@@ -111,47 +102,63 @@ static void server_routine(int client_fd, SSL *ssl) {
         int rlen = SSL_read(ssl, buf, sizeof(buf));
         if(rlen <= 0)
             break;
-        cout << "[CLIENT] " << buf;
+        cout << "[CLIENT] " << string(buf, rlen) << endl;
         int wlen = SSL_write(ssl, buf, rlen);
         assert(rlen == wlen);
     }
 }
 
 static void get_cert_key_path(bool is_server, string& ca_path, string& cer_path, string& key_path) {
-    if(is_server)
-        cout << "Server：" << endl;
-    else
-        cout << "Client：" << endl;
-
+    char buf[1024];
     string path;
-    cout << "请输入 CA 证书路径(默认为“" << DEFAULT_CA_CER_PATH << "”)：";
+
+    string prefix = is_server ? "[SERVER] " : "[CLIENT] ";
+
+    cout << prefix << "请输入 CA 证书路径(默认为“" << DEFAULT_CA_CER_PATH << "”)：";
     getline(cin, path);
     if(path.size() <= 1)
         path = DEFAULT_CA_CER_PATH;
+    realpath(path.c_str(), buf);
+    ca_path = buf;
     
     const char* defalt_cer_path = is_server ? DEFAULT_SERVER_CER_PATH : DEFAULT_CLIENT_CER_PATH;
-    cout << "请输入 私钥 文件路径(默认为“" << defalt_cer_path << "”)：";
+    cout << prefix << "请输入 证书 文件路径(默认为“" << defalt_cer_path << "”)：";
     getline(cin, path);
     if(path.size() <= 1)
         path = defalt_cer_path;
+    realpath(path.c_str(), buf);
+    cer_path = buf;
     
     const char* defalt_key_path = is_server ? DEFAULT_SERVER_KEY_PATH : DEFAULT_CLIENT_KEY_PATH;
-    cout << "请输入 私钥 文件路径(默认为“" << defalt_key_path << "”)：";
+    cout << prefix << "请输入 私钥 文件路径(默认为“" << defalt_key_path << "”)：";
     getline(cin, path);
     if(path.size() <= 1)
         path = defalt_key_path;
+    realpath(path.c_str(), buf);
+    key_path = buf;
+
+    cout << "[INFO] CA  证书路径：" << ca_path << endl;
+    cout << "[INFO] 证书 文件路径：" << cer_path << endl;
+    cout << "[INFO] 私钥 证书路径：" << key_path << endl;
 }
 
 int main(int argc, char *argv[])
 {
+    // 准备好打印调试信息
+    SSL_load_error_strings();
+    ERR_load_crypto_strings();
+    // 初始化 SSL
+    OpenSSL_add_ssl_algorithms();
+
     // 服务端 [usage] ./SSL_test <port>
     if (argc == 2)
     {
         string ca_path, cer_path, key_path;
         get_cert_key_path(true, ca_path, cer_path, key_path);
-
-        SSL_CTX* ctx = SSL_init(ca_path, cer_path, key_path);
-        SSL_CTX_set_cipher_list(ctx, "RC4-MD5");
+        // 指定 SSL 协议
+        SSL_CTX *ctx = SSL_CTX_new(TLS_server_method());
+        loadcheck_cer(ctx, ca_path, cer_path, key_path);
+        SSL_CTX_set_cipher_list(ctx, SSL_TXT_ALL);
         SSL *ssl = SSL_new(ctx);
 
         cout << "[SERVER] 监听端口 " << argv[1] << " ..." << endl;
@@ -177,13 +184,14 @@ int main(int argc, char *argv[])
         string ca_path, cer_path, key_path;
         get_cert_key_path(false, ca_path, cer_path, key_path);
 
-        SSL_CTX* ctx = SSL_init(ca_path, cer_path, key_path);
-        SSL_CTX_set_cipher_list(ctx, "RC4-MD5");
+        // 指定 SSL 协议
+        SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+        loadcheck_cer(ctx, ca_path, cer_path, key_path);
         SSL *ssl = SSL_new(ctx);
 
         cout << "[CLIENT] 尝试连接 " << argv[1] << ":" << argv[2] << " ..." << endl;
         int client_fd = connect2Server(argv[1], atoi(argv[2]));
-        if(client_fd < -1)
+        if(client_fd < 0)
             SYS_ERR("connect2Server");
         cout << "[CLIENT] 连接成功，等待用户输入..." << endl;
 
